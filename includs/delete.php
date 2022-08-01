9c3a344462634b8d3b6e08384c7aa14f70606466
hari <sauravpatel3092001@gmail.com>
2022-08-01 11:13:49 +0530

    varsion2

--- includs/delete.php
+++ includs/delete.php
@@ -1,20 +1,30 @@
 <?php
 
-$conn=mysqli_connect("localhost","root","","web");
+// $conn=mysqli_connect("localhost","root","","web");
 
-    $no = $_GET['no'];
+    // $no = $_GET['no'];
 
-    $sql = " DELETE FROM `product_add` WHERE no = $no";
+    // $sql = " DELETE FROM `product_add` WHERE no = $no";
+    
+    // if(mysqli_query($conn,$sql)){
 
-    mysqli_query($conn,$sql);
+    // header('location:admin.php');
+    // }
+    // else{
+    //     header('location:admin.php');
+    // }
 
-    header('location:admin.php');
 
+    $conn=mysqli_connect("localhost","root","","admin");
     $id = $_GET['id'];
 
     $sql1= " DELETE FROM `slider_tbl` WHERE id = $id";
 
-    mysqli_query($conn,$sql1);
+    if(mysqli_query($conn,$sql1)){
 
-    header('location:pages/slider.php');
+    header('location:slider.php');
+}else{
+    header('location:admin.php');
+    }
+
     ?>
